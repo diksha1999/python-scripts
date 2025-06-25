0cf1024af9335ea8e3382824b651d9e130bd907a
Diksha Sharma <1740dikshasharma@gmail.com>
2025-06-25 08:29:46 +0530

    new changes

--- sys_module.py
+++ sys_module.py
@@ -1,11 +1,16 @@
 import sys
 
-# he sys module provides access to system-specific parameters and functions. 
+# The sys module provides access to system-specific parameters and functions. 
 # Besides sys.argv (for command-line arguments), sys.platform (to get OS type), and sys.exit() (to exit the script)
 
 print(sys.path)
 # `sys.path` is a built-in list in Python that tells the interpreter where to look for modules to import.
 # It’s part of the `sys` module, which provides access to some variables and functions related to the Python interpreter.
+
+
+# How Does `sys.path` Work?
+# When you run `import some_module` in Python, the interpreter searches for `some_module` in the directories listed in `sys.path`.
+# It checks each directory in order until it finds the module or raises an `ImportError`
 
 print(f"This is the python interpretor version: {sys.version}")
 
